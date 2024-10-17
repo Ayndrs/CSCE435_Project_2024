fa4cfdfa292352b2f6c29d933e0acea3978789ab
Bush <64496558+Ayndrs@users.noreply.github.com>
2024-10-16 22:24:30 -0500

    merge algorithm update

--- Algorithms/Merge/merge_sort.cpp
+++ Algorithms/Merge/merge_sort.cpp
@@ -7,55 +7,119 @@
 #include <adiak.hpp>
 #include <math.h>
 
-#define MASTER 0
-#define FROM_MASTER 1
-#define FROM_WORKER 2
-
-//functions
-void merge(int *array, int l, int m, int r);
-void mergeSort(int *array, int l, int r);
+void merge_arrays(int *, int *, int, int, int);
+void merge_sort(int *, int *, int, int);
 void check(int *array, int sizeOfArray);
 
-//metadata
-const char *algorithm = "merge";
-const char *programming_model = "mpi";
-const char *data_type = "int";
-const char *input_type = "Random";
-const char *implementation_source = "online";
-const char *scalability = "weak"; 
-const size_t size_of_data_type = sizeof(int);
-
-int main (int argc, char *argv[]) {
+int main(int argc, char** argv) {
     CALI_CXX_MARK_FUNCTION;
 
-    int sizeOfArray;
-    if (argc == 2) {
-        sizeOfArray = atoi(argv[1]);
-    } 
-    else {
-        printf("\nPlease provide the size of the array\n");
-        return 0;
-    }
-  
+    //metadata variables
+    const char *algorithm = "merge";
+    const char *programming_model = "mpi";
+    const char *data_type = "int";
+    const char *input_type = "Random";
+    const char *implementation_source = "online";
+    const char *scalability = "weak"; 
+    const size_t size_of_data_type = sizeof(int);
 
-    int numtasks, taskid, numworkers, source, dest, mtype, averow, extra, offset, i;
-    int *array = NULL, *local_array = NULL;
     const char* main = "main";
     const char* data_init_runtime = "data_init_runtime";
     const char* comm = "comm";
-    const char* comm_small = "comm_small";
     const char* comm_large = "comm_large";
     const char* comp = "comp";
-    const char* comp_small = "comp_small";
     const char* comp_large = "comp_large";
     const char* correctness_check = "correctness_check";
+   
+    //declare array
+    int array_size = 0;
+    if (argc == 2) {
+        array_size = atoi(argv[1]);
+    } else {
+        printf("\nProvide the size of the array\n");
+        return 0;
+    }
+    
+    int process_rank;
+    int total_processes;
 
     MPI_Status status;
-
     MPI_Init(&argc, &argv);
-    MPI_Comm_rank(MPI_COMM_WORLD, &taskid);
-    MPI_Comm_size(MPI_COMM_WORLD, &numtasks);
+    MPI_Comm_rank(MPI_COMM_WORLD, &process_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &total_processes);
 
+    CALI_MARK_BEGIN("main");
+  
+    cali::ConfigManager mgr;
+    mgr.start();
+  
+    //initialize array
+    CALI_MARK_BEGIN("data_init_runtime");
+    int *global_array = (int*)malloc(array_size * sizeof(int));
+    int index;
+    srand(time(NULL));
+    for(index = 0; index < array_size; index++) {
+        global_array[index] = rand() % 1000;    
+    }
+    CALI_MARK_END("data_init_runtime");
+   
+    //divide array
+    int local_chunk_size = array_size / total_processes;
+
+    //send subarrays using mpi scatter
+    CALI_MARK_BEGIN("comm");
+    CALI_MARK_BEGIN("comm_large");
+    int *local_array = (int*)malloc(local_chunk_size * sizeof(int));
+    MPI_Scatter(global_array, local_chunk_size, MPI_INT, local_array, local_chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    CALI_MARK_END("comm_large");
+    CALI_MARK_END("comm");
+    
+    
+    
+    CALI_MARK_BEGIN("comp");
+    CALI_MARK_BEGIN("comp_large");
+    int *temp_array = (int*)malloc(local_chunk_size * sizeof(int));
+    merge_sort(local_array, temp_array, 0, local_chunk_size - 1);
+    CALI_MARK_END("comp_large");
+    CALI_MARK_END("comp");
+  
+    //combine subarrays
+    int *final_sorted_array = NULL;
+    if (process_rank == 0) {
+        final_sorted_array = (int*)malloc(array_size * sizeof(int));
+    }
+
+    CALI_MARK_BEGIN("comm");
+    CALI_MARK_BEGIN("comm_large");
+    if (process_rank == 0) {
+        MPI_Gather(MPI_IN_PLACE, local_chunk_size, MPI_INT, final_sorted_array, local_chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    } else {
+        MPI_Gather(local_array, local_chunk_size, MPI_INT, NULL, local_chunk_size, MPI_INT, 0, MPI_COMM_WORLD);
+    }
+    CALI_MARK_END("comm_large");
+    CALI_MARK_END("comm");
+ 
+    //merge sort on the master
+    if (process_rank == 0) {
+        CALI_MARK_BEGIN("comp");
+        CALI_MARK_BEGIN("comp_large");
+        int *final_temp_array = (int*)malloc(array_size * sizeof(int));
+        merge_sort(final_sorted_array, final_temp_array, 0, array_size - 1);
+        CALI_MARK_END("comp_large");
+        CALI_MARK_END("comp");
+
+        check(final_sorted_array, array_size);
+        
+        free(final_sorted_array);
+        free(final_temp_array);
+    }
+ 
+    CALI_MARK_BEGIN("main");
+
+    free(global_array);
+    free(local_array);
+    free(temp_array);
+ 
     adiak::init(NULL);
     adiak::launchdate();
     adiak::libraries();
@@ -65,172 +129,65 @@ int main (int argc, char *argv[]) {
     adiak::value("programming_model", programming_model);
     adiak::value("data_type", data_type);
     adiak::value("size_of_data_type", size_of_data_type);
-    adiak::value("input_size", sizeOfArray);
+    adiak::value("input_size", array_size);
     adiak::value("input_type", input_type); 
-    adiak::value("num_procs", numtasks); 
+    adiak::value("num_procs", total_processes); 
     adiak::value("scalability", scalability); 
     adiak::value("group_num", 22); 
     adiak::value("implementation_source", implementation_source);
-
-
-    if (numtasks < 2) {
-        printf("Need at least two MPI tasks. Quitting...\n");
-        MPI_Abort(MPI_COMM_WORLD, 1);
-        exit(1);
-    }
-    numworkers = numtasks - 1;
-
-    double start_time, end_time, local_time, total_local_time, min_time, max_time, avg_time, sum_squared_diffs, variance_time;
-
-    start_time = MPI_Wtime(); 
-    CALI_MARK_BEGIN("main");
-
-    cali::ConfigManager mgr;
-    mgr.start();
-
-    //master
-    if (taskid == MASTER) {
-        CALI_MARK_BEGIN("data_init_runtime");
-
-        array = (int*)malloc(sizeOfArray * sizeof(int));
-        for (i = 0; i < sizeOfArray; i++) {
-            array[i] = rand() % 1000; 
-        }
-        CALI_MARK_END("data_init_runtime");
-
-        CALI_MARK_BEGIN("comm");
-        CALI_MARK_BEGIN("comm_large");
-        averow = sizeOfArray / numworkers;
-        extra = sizeOfArray % numworkers;
-        offset = 0;
-        mtype = FROM_MASTER;
-        for (dest = 1; dest <= numworkers; dest++) {
-            int chunk_size = (dest <= extra) ? averow + 1 : averow;
-            MPI_Send(&offset, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            MPI_Send(&chunk_size, 1, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            MPI_Send(&array[offset], chunk_size, MPI_INT, dest, mtype, MPI_COMM_WORLD);
-            offset += chunk_size;
-        }
-
-        mtype = FROM_WORKER;
-        for (i = 1; i <= numworkers; i++) {
-            source = i;
-            int chunk_size;
-            MPI_Recv(&offset, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-            MPI_Recv(&chunk_size, 1, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-            MPI_Recv(&array[offset], chunk_size, MPI_INT, source, mtype, MPI_COMM_WORLD, &status);
-        }
-        CALI_MARK_END("comm_large");
-        CALI_MARK_END("comm");
-
-        CALI_MARK_BEGIN("comp");
-        CALI_MARK_BEGIN("comp_large");
-        mergeSort(array, 0, sizeOfArray - 1);
-        CALI_MARK_END("comp_large");
-        CALI_MARK_END("comp");
-
-        check(array, sizeOfArray);
-        free(array);
-    }
-
-    //worker
-    if (taskid > MASTER) {
-        CALI_MARK_BEGIN("comm");
-        CALI_MARK_BEGIN("comm_large");
-        mtype = FROM_MASTER;
-        int chunk_size;
-        MPI_Recv(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        MPI_Recv(&chunk_size, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-
-        local_array = (int *)malloc(chunk_size * sizeof(int));
-        MPI_Recv(local_array, chunk_size, MPI_INT, MASTER, mtype, MPI_COMM_WORLD, &status);
-        CALI_MARK_END("comm_large");
-        CALI_MARK_END("comm");
-
-
-        CALI_MARK_BEGIN("comp");
-        CALI_MARK_BEGIN("comp_large");
-        mergeSort(local_array, 0, chunk_size - 1);
-        CALI_MARK_END("comp_large");
-        CALI_MARK_END("comp");
-
-        mtype = FROM_WORKER;
-        MPI_Send(&offset, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-        MPI_Send(&chunk_size, 1, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-        MPI_Send(local_array, chunk_size, MPI_INT, MASTER, mtype, MPI_COMM_WORLD);
-
-        free(local_array);
-    }
-    end_time = MPI_Wtime();
-    local_time = end_time - start_time;
-
-    CALI_MARK_END("main");
-
-    MPI_Reduce(&local_time, &min_time, 1, MPI_DOUBLE, MPI_MIN, MASTER, MPI_COMM_WORLD);
-    MPI_Reduce(&local_time, &max_time, 1, MPI_DOUBLE, MPI_MAX, MASTER, MPI_COMM_WORLD);
-    MPI_Reduce(&local_time, &total_local_time, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
-    avg_time = total_local_time / numtasks;
-
-    double local_squared_diff = (local_time - avg_time) * (local_time - avg_time);
-    MPI_Reduce(&local_squared_diff, &sum_squared_diffs, 1, MPI_DOUBLE, MPI_SUM, MASTER, MPI_COMM_WORLD);
-    variance_time = sum_squared_diffs / numtasks;
-
-    if (taskid == MASTER) {
-        printf("Total Time: %f\n", total_local_time);
-        printf("Min Time/Rank: %f\n", min_time);
-        printf("Max Time/Rank: %f\n", max_time);
-        printf("Avg Time/Rank: %f\n", avg_time);
-        printf("Variance Time/Rank: %f\n", variance_time);
-    }
-
+ 
+    MPI_Barrier(MPI_COMM_WORLD);
     mgr.stop();
     mgr.flush();
     MPI_Finalize();
     return 0;
 }
 
-void merge(int *array, int l, int m, int r) {
-    int n1 = m - l + 1;
-    int n2 = r - m;
-    int *L = (int *)malloc(n1 * sizeof(int));
-    int *R = (int *)malloc(n2 * sizeof(int));
+void merge_arrays(int *original, int *temp, int left, int middle, int right) {
+    int left_index = left;        
+    int right_index = middle + 1; 
+    int merge_index = left;       
 
-    for (int i = 0; i < n1; i++) L[i] = array[l + i];
-    for (int j = 0; j < n2; j++) R[j] = array[m + 1 + j];
-
-    int i = 0, j = 0, k = l;
-    while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
-            array[k] = L[i++];
+    while (left_index <= middle && right_index <= right) {
+        if (original[left_index] <= original[right_index]) {
+            temp[merge_index] = original[left_index];
+            left_index++;
         } else {
-            array[k] = R[j++];
+            temp[merge_index] = original[right_index];
+            right_index++;
         }
-        k++;
+        merge_index++;
     }
 
-    while (i < n1){
-      array[k++] = L[i++];
-    }
-    while (j < n2) {
-      array[k++] = R[j++];
+    while (left_index <= middle) {
+        temp[merge_index] = original[left_index];
+        left_index++;
+        merge_index++;
     }
 
-    free(L);
-    free(R);
-}
+    while (right_index <= right) {
+        temp[merge_index] = original[right_index];
+        right_index++;
+        merge_index++;
+    }
 
-void mergeSort(int *array, int l, int r) {
-    if (l < r) {
-        int m = l + (r - l) / 2;
-        mergeSort(array, l, m);
-        mergeSort(array, m + 1, r);
-        merge(array, l, m, r);
+    for (int i = left; i <= right; i++) {
+        original[i] = temp[i];  
     }
 }
 
-void check(int *array, int sizeOfArray) {
+void merge_sort(int *original, int *temp, int left, int right) {
+    if (left < right) {
+        int middle = (left + right) / 2;
+        merge_sort(original, temp, left, middle);
+        merge_sort(original, temp, middle + 1, right);
+        merge_arrays(original, temp, left, middle, right);
+    }
+}
+
+void check(int *array, int size_of_array) {
     CALI_MARK_BEGIN("correctness_check");
-    for (int i = 1; i < sizeOfArray; i++) {
+    for (int i = 1; i < size_of_array; i++) {
         if (array[i - 1] > array[i]) {
             printf("Array is not sorted at index %d\n", i);
             return;
